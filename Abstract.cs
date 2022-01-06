5e809f7f5d8398abcd2a95b8b739a2ff456f5737
sowjanya.gollapathi <sowjanya.gollapathi@gmail.com>
2022-01-06 18:14:40 +0530

    Add project files.

---
+++ Abstract.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp6
+{
+    abstract class AbsClass
+    {
+        public int AddTwoNumbers(int Num1, int Num2)
+        {
+            return Num1 + Num2;
+        }
+        public abstract int MultiplyTwoNumbers(int Num1, int Num2);
+    }
+    class AbsDerived : AbsClass
+    {
+        static void Main(string[] args)
+        {
+            AbsDerived calculate = new AbsDerived();
+            int added = calculate.AddTwoNumbers(10, 20);
+            int multiplied = calculate.MultiplyTwoNumbers(10, 20);
+            Console.WriteLine("Added : {0}, Multiplied : {1}", added, multiplied);
+        }
+        public override int MultiplyTwoNumbers(int Num1, int Num2)
+        {
+            return Num1 * Num2;
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp6
+{
+    public class Product : IProduct
+    {
+        public string GetProductName()
+        {
+            return "Moto G5";
+        }
+        public string GetProductPrice()
+        {
+            return "2500";
+        }
+    }
+}
